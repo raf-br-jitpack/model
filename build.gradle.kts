25e11e6a1f94f2fa9e952a111c1cf30b84d5aa29
Rafał Świerzyński <rafal.swierzynski@jambit.com>
2021-09-01 15:29:04 +0200

    Always build for 64-bit iOS

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform") version "1.5.30"
     `maven-publish`
@@ -16,13 +18,13 @@ kotlin {
             kotlinOptions.jvmTarget = "11"
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+
+    iosX64("ios") {
+        binaries {
+            framework {
+                baseName = "model"
+            }
+        }
     }
 
     sourceSets {
@@ -30,7 +32,7 @@ kotlin {
         val commonTest by getting
         val jvmMain by getting
         val jvmTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
+        val iosMain by getting
+        val iosTest by getting
     }
 }
